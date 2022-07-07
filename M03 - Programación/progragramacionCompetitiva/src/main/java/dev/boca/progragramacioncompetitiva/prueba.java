ffc6d896077f05818201e3d935050aa17c52e393
abocadev <albert.bocanegra2003@gmail.com>
2022-07-07 11:13:31 +0100

    asdf

---
+++ M03 - Programación/progragramacionCompetitiva/src/main/java/dev/boca/progragramacioncompetitiva/prueba.java
@@ -0,0 +1,15 @@
+package dev.boca.progragramacioncompetitiva;
+public class prueba {
+    public static void main(String[] args) {
+        String[] elementos = {"hola", "adios", "hello", "good", "bye"};
+        for(String i: elementos){
+            System.out.println(i);
+        }
+        
+        int number = 1;
+        String resultado = number == 0 ? "verdadero" : "falso";
+        
+        System.out.println(resultado);
+    }
+    
+}
