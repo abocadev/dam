e0402e05e28342fb1acdb2ca5a4ebc20066d53ba
abocadev <albert.bocanegra2003@gmail.com>
2022-09-24 14:34:50 +0200

    Update 24-09-2022

---
+++ M08 - Programacion Multimedia y Movil/Ejercicio1_UF1_BocanegraBarreiroAlbert/app/src/main/java/dev/boca/ejercicio1_uf1_bocanegrabarreiroalbert/MainActivity.java
@@ -0,0 +1,13 @@
+package dev.boca.ejercicio1_uf1_bocanegrabarreiroalbert;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
