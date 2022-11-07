f992e2971562c1b7678e8c9988361f2deabe8535
abocadev <albert.bocanegra2003@gmail.com>
2022-11-07 22:54:39 +0100

    as

--- M08 - Programacion Multimedia y Movil/ClickInImg/app/src/main/java/dev/boca/clickinimg/MainActivity.java
+++ M08 - Programacion Multimedia y Movil/ClickInImg/app/src/main/java/dev/boca/clickinimg/MainActivity.java
@@ -2,13 +2,46 @@ package dev.boca.clickinimg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "dev.boca.clickinimg.extra.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void displayToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void showDonutOrder(View view){
+        displayToast(getString(R.string.donut_order_message));
+    }
+
+    public void showIceCreamOrder(View view){
+        displayToast(getString(R.string.ice_cream_order_message));
+    }
+
+    public void showFroyoOrder(View view){
+        displayToast(getString(R.string.froyo_order_message));
     }
 }
