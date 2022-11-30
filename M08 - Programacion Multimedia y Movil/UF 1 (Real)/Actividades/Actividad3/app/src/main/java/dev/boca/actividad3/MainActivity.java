2b6c43f97643b7c9fdf1bdaba4f8f2373ee28b30
Albert Bocanegra Barreiro <71650816+abocadev@users.noreply.github.com>
2022-11-30 22:29:10 +0100

    upd

--- M08 - Programacion Multimedia y Movil/UF 1 (Real)/Actividades/Actividad3/app/src/main/java/dev/boca/actividad3/MainActivity.java
+++ M08 - Programacion Multimedia y Movil/UF 1 (Real)/Actividades/Actividad3/app/src/main/java/dev/boca/actividad3/MainActivity.java
@@ -1,14 +1,32 @@
 package dev.boca.actividad3;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void setSupportActionBar(Toolbar supportActionBar) {
+        this.toolbar = supportActionBar;
     }
 }
