a1fc876fa13364aa8bf494fff9194a5ad8ea2737
abocadev <albert.bocanegra2003@gmail.com>
2022-12-08 21:53:18 +0100

    Update

---
+++ M08 - Programacion Multimedia y Movil/UF 1 (Real)/CodeLabs/Parte 4/4.5/RecyclerView/app/src/main/java/dev/boca/recyclerview/wordlist_item.java
@@ -0,0 +1,14 @@
+package dev.boca.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class wordlist_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wordlist_item);
+    }
+}
