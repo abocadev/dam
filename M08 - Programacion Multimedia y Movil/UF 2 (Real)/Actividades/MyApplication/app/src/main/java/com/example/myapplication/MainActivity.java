7983339d54d6e2a15e6c7a41ca81acc918502370
abocadev <albert.bocanegra2003@gmail.com>
2023-02-24 12:29:54 +0100

    update

--- M08 - Programacion Multimedia y Movil/UF 2 (Real)/Actividades/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ M08 - Programacion Multimedia y Movil/UF 2 (Real)/Actividades/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,77 +1,73 @@
-package dev.boca.actividad3;
+package com.example.myapplication;
 
 import static android.Manifest.permission.RECORD_AUDIO;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean presionado;
-    MediaRecorder md;
+    public static boolean presionado = false;
+
+    public static MediaRecorder md;
     public static final int REQUEST_AUDIO_PERMISSION_CODE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         RequestsPermissions();
-        try {
-            String filename = "myFile.txt";
-            String content = "Hola, mundo!";
-            FileOutputStream fos = openFileOutput(filename, Context.MODE_PRIVATE);
-            fos.write(content.getBytes());
-            fos.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
-//        Toast.makeText(getApplicationContext(), Environment.getExternalStorageDirectory().getAbsolutePath(), Toast.LENGTH_LONG).show();
-//        Button button = findViewById(R.id.button);
-//
-//        presionado = false;
+        Toast.makeText(getApplicationContext(), Environment.getExternalStorageDirectory().getAbsolutePath(), Toast.LENGTH_LONG).show();
+        Button button = findViewById(R.id.button);
+
 //        button.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
 //
+//                md = new MediaRecorder();
+//                md.setAudioSource(MediaRecorder.AudioSource.MIC);
+//                md.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+//                md.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+//                md.setOutputFile(Environment.getExternalStorageDirectory().getAbsolutePath() + "/audio.3gp");
 //
+//                Toast.makeText(MainActivity.this, "The button is clicked", Toast.LENGTH_SHORT).show();
 //                if(!presionado){
-//                    md = new MediaRecorder();
-//                    md.setAudioSource(MediaRecorder.AudioSource.MIC);
-//                    md.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-//                    md.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-//                    md.setOutputFile(Environment.getExternalStorageDirectory().getAbsolutePath() + "/audio.3gp");
-//
-//
 //                    try {
 //                        md.prepare();
-//                        md.start();
-//                    }catch (Exception e){
-//                        e.printStackTrace();
+//                    } catch (IOException e) {
+//                        throw new RuntimeException(e);
 //                    }
-//
+//                    md.start();
 //                    presionado = true;
+//                    Toast.makeText(MainActivity.this, "Esta grabando", Toast.LENGTH_LONG).show();
 //                }else{
 //                    try {
 //                        md.stop();
 //                        md.release();
 //                        presionado = false;
+//                        Toast.makeText(MainActivity.this, "La grabacion se ha parado", Toast.LENGTH_LONG).show();
 //                    }catch (Exception e){
 //                        e.printStackTrace();
 //                    }
 //                }
-//                Toast.makeText(MainActivity.this, "The button is clicked", Toast.LENGTH_SHORT).show();
 //            }
 //        });
     }
